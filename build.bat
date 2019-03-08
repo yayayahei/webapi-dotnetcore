cac89e7f0c1957126d526242d337a0237b3a7076
yayayahei <capgemini.w.li@daimler.com>
2019-03-08 11:22:42 +0800

    modify build script

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
 @echo off
 cd %~dp0
-setlocal
 rd /s/q deploy
-dotnet publish ./Hello/Hello.csproj -c Release -o ../deploy 
-endlocal
+dotnet publish ./src/Hello/Hello.csproj -c Release -o ../../deploy 
